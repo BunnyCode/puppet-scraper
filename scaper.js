a6fb3b0f74579476f711557010961975f83be984
Theodor Borgman <theodor.borgman@gmail.com>
2024-03-25 00:11:43 +0100

    Get top 5 results

--- scaper.js
+++ scaper.js
@@ -7,14 +7,20 @@ const diffbotApiKey = process.env.DIFFBOT_API_KEY;
 
 async function searchWithDiff(query) {
   sdk.auth(diffbotApiKey);
+  const fixedQuery = query.split(" ").join("+");
   const firstResultUrl = await diffSearch.search(
-    "https://www.google.com/search?q=alexei+navalny+died+in+prison"
+    `https://www.google.com/search?q=${fixedQuery}`
   );
 
   if (firstResultUrl) {
-    console.log(`Navigating to first result: ${firstResultUrl}`);
+    console.log(`Getting Results`);
 
-    console.log(firstResultUrl);
+    // console.log(firstResultUrl);
+    const arrLen = firstResultUrl.objects[0].items.length;
+    const adjustedLen = arrLen > 5 ? 5 : arrLen;
+    const topFiveHits = firstResultUrl.objects[0].items.slice(0, adjustedLen);
+
+    console.log(topFiveHits);
   } else {
     console.log("Failed to find the first result's URL.");
   }
