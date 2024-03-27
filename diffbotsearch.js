803e89025184de7e6dc69cc9ab60fa4affda70d5
Fredrik Jurgell <fj222qp@student.lnu.se>
2024-03-27 11:55:04 +0100

    Remove token

--- diffbotsearch.js
+++ diffbotsearch.js
@@ -2,19 +2,16 @@ require("dotenv").config();
 const sdk = require("api")("@diffbot-2/v1.1#9i9y4qmlr6p26mz");
 const diffbotApiKey = process.env.DIFFBOT_API_KEY;
 class DiffbotWebSearch {
-  search(url) {
-    sdk.auth(diffbotApiKey);
-    return sdk
-      .list({ url })
-      .then(({ data }) => {
-        // Extract the data returned as a variable
-        const extractedData = data;
-        return extractedData;
-      })
-      .catch((err) => {
-        console.error(err);
-        throw err;
-      });
+  async search(url) {
+     url = 'https://www.google.com/search?q=biden+is+lost&scroll=slow';
+const token = diffbotApiKey;
+const apiUrl = `https://api.diffbot.com/v3/list?url=${encodeURIComponent(url)}&token=${token}`;
+
+fetch(apiUrl)
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error('Error:', error));
+
   }
 
   article(url) {
